122e33261a16c201fa9424a62fc6403f33108d75
fmstacco <fmstacco@gmail.com>
2023-06-26 08:52:16 +0000

    corrected path for 404 and 500 template

--- its4kids/views.py
+++ its4kids/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
+    return render(request, "404.html", status=404)
 
 
 def handler500(request):
     """ Handles error pages 500 """
 
-    return render(request, 'errors/500.html', status=500)
+    return render(request, '500.html', status=500)
